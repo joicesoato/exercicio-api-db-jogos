eac55c5dd96375468273319eef1d9fec0580ac98
Joice Soato <soatojoyce@gmail.com>
2026-05-25 19:34:05 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import "./db"
+import generosRouter from "./routes/generos";
+import livrosRouter from "./routes/livros"
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use("/generos", generosRouter);
+app.use("/livros", livrosRouter);
+
+app.listen(PORT, () => {
+    console.log(`Servidor executando em localhost:${PORT}`)
+});
